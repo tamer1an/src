688b8fc8adac5959da13702d474c07ad76ed8dd0
Trybynenko Andrii <trybynenko@gmail.com>
2014-03-26 17:36:15 +0200

    basic

---
+++ hello.go
@@ -0,0 +1,19 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: next
+ * Date: 26.03.14
+ * Time: 17:11
+ * To change this template use File | Settings | File Templates.
+ */
+package main
+
+import "fmt"
+
+func main() {
+    var msg string
+    msg = "myMsg"
+
+    var a,b,c int
+
+    fmt.Println(msg,a,b,c)
+}
